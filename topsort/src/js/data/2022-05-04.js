3a0d16bab3556cf09be9cd3a797d45bfe2412692
momorin90 <ad20171217@gmail.com>
2022-05-04 20:59:05 +0200

    Added sorter for Top 100

---
+++ topsort/src/js/data/2022-05-04.js
@@ -0,0 +1,511 @@
+dataSetVersion = "2022-05-04"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Special Filter",
+    key: "special",
+    tooltip: "Check this to restrict to a certain group of idols.",
+    checked: false,
+    sub: [{ name: "Top 100", key: "top100" }],
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Arin",
+    img: "1.jpg",
+    opts: { special: ["top100"], groups: ["Oh My Girl"] },
+  },
+  {
+    name: "Bona",
+    img: "2.jpg",
+    opts: { special: ["top100"], groups: ["WJSN"] },
+  },
+  {
+    name: "Chaehyun",
+    img: "3.jpg",
+    opts: { special: ["top100"], groups: ["Kep1er"] },
+  },
+  {
+    name: "Chaewon",
+    img: "4.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE", "LE SSERAFIM"] },
+  },
+  {
+    name: "Chaeyoung",
+    img: "5.jpg",
+    opts: { special: ["top100"], groups: ["fromis_9"] },
+  },
+  {
+    name: "Chaeyoung",
+    img: "6.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Cheng Xiao",
+    img: "7.jpg",
+    opts: { special: ["top100"], groups: ["WJSN"] },
+  },
+  {
+    name: "Choa",
+    img: "8.jpg",
+    opts: { special: ["top100"], groups: ["AOA"] },
+  },
+  {
+    name: "Chungha",
+    img: "9.jpg",
+    opts: { special: ["top100"], groups: ["I.O.I"] },
+  },
+  {
+    name: "Chuu",
+    img: "10.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Dahyun",
+    img: "11.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Dajeong",
+    img: "12.jpg",
+    opts: { special: ["top100"], groups: ["PIXY"] },
+  },
+  {
+    name: "Dasom",
+    img: "13.jpg",
+    opts: { special: ["top100"], groups: ["Sistar"] },
+  },
+  {
+    name: "Do-A (ex Bella)",
+    img: "14.jpg",
+    opts: { special: ["top100"], groups: ["Alice (ex Elris)"] },
+  },
+  {
+    name: "Dosie",
+    img: "15.jpg",
+    opts: { special: ["top100"], groups: ["Purple Kiss"] },
+  },
+  {
+    name: "E:U",
+    img: "16.jpg",
+    opts: { special: ["top100"], groups: ["Everglow"] },
+  },
+  {
+    name: "Eunbin",
+    img: "17.jpg",
+    opts: { special: ["top100"], groups: ["CLC"] },
+  },
+  {
+    name: "Gahyeon",
+    img: "18.jpg",
+    opts: { special: ["top100"], groups: ["Dreamcatcher"] },
+  },
+  {
+    name: "Gyuri",
+    img: "19.jpg",
+    opts: { special: ["top100"], groups: ["fromis_9"] },
+  },
+  {
+    name: "Hani",
+    img: "20.jpg",
+    opts: { special: ["top100"], groups: ["EXID"] },
+  },
+  {
+    name: "Haseul",
+    img: "21.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Hayoung",
+    img: "22.jpg",
+    opts: { special: ["top100"], groups: ["fromis_9"] },
+  },
+  {
+    name: "Heejin",
+    img: "23.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Hyeri",
+    img: "24.jpg",
+    opts: { special: ["top100"], groups: ["Girl's Day"] },
+  },
+  {
+    name: "Hyewon",
+    img: "25.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE"] },
+  },
+  {
+    name: "Hyunjin",
+    img: "26.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Irene",
+    img: "27.jpg",
+    opts: { special: ["top100"], groups: ["Red Velvet"] },
+  },
+  { name: "IU", img: "28.jpg", opts: { special: ["top100"], groups: [""] } },
+  {
+    name: "Jennie",
+    img: "29.jpg",
+    opts: { special: ["top100"], groups: ["Blackpink"] },
+  },
+  {
+    name: "Jihan",
+    img: "30.jpg",
+    opts: { special: ["top100"], groups: ["Weeekly"] },
+  },
+  {
+    name: "Jiho",
+    img: "31.jpg",
+    opts: { special: ["top100"], groups: ["Oh My Girl"] },
+  },
+  {
+    name: "Jihyo",
+    img: "32.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Jisoo",
+    img: "33.jpg",
+    opts: { special: ["top100"], groups: ["Lovelyz"] },
+  },
+  {
+    name: "Jisoo",
+    img: "34.jpg",
+    opts: { special: ["top100"], groups: ["Blackpink"] },
+  },
+  {
+    name: "JiU",
+    img: "35.jpg",
+    opts: { special: ["top100"], groups: ["Dreamcatcher"] },
+  },
+  {
+    name: "Jiwon",
+    img: "36.jpg",
+    opts: { special: ["top100"], groups: ["Cherry Bullet"] },
+  },
+  {
+    name: "Joy",
+    img: "37.jpg",
+    opts: { special: ["top100"], groups: ["Red Velvet"] },
+  },
+  {
+    name: "Karina",
+    img: "38.jpg",
+    opts: { special: ["top100"], groups: ["aespa"] },
+  },
+  {
+    name: "Lisa",
+    img: "39.jpg",
+    opts: { special: ["top100"], groups: ["Blackpink"] },
+  },
+  {
+    name: "Luda",
+    img: "40.jpg",
+    opts: { special: ["top100"], groups: ["WJSN"] },
+  },
+  {
+    name: "May",
+    img: "41.jpg",
+    opts: { special: ["top100"], groups: ["Cherry Bullet"] },
+  },
+  {
+    name: "Mijoo",
+    img: "42.jpg",
+    opts: { special: ["top100"], groups: ["Lovelyz"] },
+  },
+  {
+    name: "Mina",
+    img: "43.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Minju",
+    img: "44.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE"] },
+  },
+  {
+    name: "Miyeon",
+    img: "45.jpg",
+    opts: { special: ["top100"], groups: ["(G)I-DLE"] },
+  },
+  {
+    name: "Momo",
+    img: "46.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Monday",
+    img: "47.jpg",
+    opts: { special: ["top100"], groups: ["Weeekly"] },
+  },
+  {
+    name: "Nakyung",
+    img: "48.jpg",
+    opts: { special: ["top100"], groups: ["fromis_9"] },
+  },
+  {
+    name: "Nana",
+    img: "49.jpg",
+    opts: { special: ["top100"], groups: ["After School", "Orange Caramel"] },
+  },
+  {
+    name: "Nancy",
+    img: "50.jpg",
+    opts: { special: ["top100"], groups: ["Momoland"] },
+  },
+  {
+    name: "Nayeon",
+    img: "51.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Nayoung",
+    img: "52.jpg",
+    opts: { special: ["top100"], groups: ["I.O.I", "Pristin"] },
+  },
+  {
+    name: "Ningning",
+    img: "53.jpg",
+    opts: { special: ["top100"], groups: ["aespa"] },
+  },
+  {
+    name: "Olivia Hye",
+    img: "54.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Onda",
+    img: "55.jpg",
+    opts: { special: ["top100"], groups: ["Everglow"] },
+  },
+  {
+    name: "Rei",
+    img: "56.jpg",
+    opts: { special: ["top100"], groups: ["IVE"] },
+  },
+  {
+    name: "Rosé",
+    img: "57.jpg",
+    opts: { special: ["top100"], groups: ["Blackpink"] },
+  },
+  {
+    name: "Ryujin",
+    img: "58.jpg",
+    opts: { special: ["top100"], groups: ["ITZY"] },
+  },
+  {
+    name: "Saerom",
+    img: "59.jpg",
+    opts: { special: ["top100"], groups: ["fromis_9"] },
+  },
+  {
+    name: "Sakura",
+    img: "60.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE", "LE SSERAFIM"] },
+  },
+  {
+    name: "Sana",
+    img: "61.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Seeun",
+    img: "62.jpg",
+    opts: { special: ["top100"], groups: ["STAYC"] },
+  },
+  {
+    name: "Sejeong",
+    img: "63.jpg",
+    opts: { special: ["top100"], groups: ["I.O.I", "Gugudan"] },
+  },
+  {
+    name: "Seolhyun",
+    img: "64.jpg",
+    opts: { special: ["top100"], groups: ["AOA"] },
+  },
+  {
+    name: "Seulgi",
+    img: "65.jpg",
+    opts: { special: ["top100"], groups: ["Red Velvet"] },
+  },
+  {
+    name: "Shuhua",
+    img: "66.jpg",
+    opts: { special: ["top100"], groups: ["(G)I-DLE"] },
+  },
+  {
+    name: "Sihyeon",
+    img: "67.jpg",
+    opts: { special: ["top100"], groups: ["Everglow"] },
+  },
+  {
+    name: "Soeun",
+    img: "68.jpg",
+    opts: { special: ["top100"], groups: ["Weeekly"] },
+  },
+  {
+    name: "Sohee",
+    img: "69.jpg",
+    opts: { special: ["top100"], groups: ["Alice (ex Elris)"] },
+  },
+  {
+    name: "Somi",
+    img: "70.jpg",
+    opts: { special: ["top100"], groups: ["I.O.I"] },
+  },
+  {
+    name: "Soojin",
+    img: "71.jpg",
+    opts: { special: ["top100"], groups: ["(G)I-DLE"] },
+  },
+  {
+    name: "Sowon",
+    img: "72.jpg",
+    opts: { special: ["top100"], groups: ["GFriend"] },
+  },
+  {
+    name: "Sumin",
+    img: "73.jpg",
+    opts: { special: ["top100"], groups: ["STAYC"] },
+  },
+  {
+    name: "Sunmi",
+    img: "74.jpg",
+    opts: { special: ["top100"], groups: ["Wonder Girls"] },
+  },
+  {
+    name: "Suyun",
+    img: "75.jpg",
+    opts: { special: ["top100"], groups: ["Rocket Punch"] },
+  },
+  {
+    name: "Suzy",
+    img: "76.jpg",
+    opts: { special: ["top100"], groups: ["Miss A"] },
+  },
+  {
+    name: "Taeyeon",
+    img: "77.jpg",
+    opts: { special: ["top100"], groups: ["SNSD"] },
+  },
+  {
+    name: "Tiffany",
+    img: "78.jpg",
+    opts: { special: ["top100"], groups: ["SNSD"] },
+  },
+  {
+    name: "Tzuyu",
+    img: "79.jpg",
+    opts: { special: ["top100"], groups: ["Twice"] },
+  },
+  {
+    name: "Wendy",
+    img: "80.jpg",
+    opts: { special: ["top100"], groups: ["Red Velvet"] },
+  },
+  {
+    name: "Wonyoung",
+    img: "81.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE", "IVE"] },
+  },
+  {
+    name: "Xiaoting",
+    img: "82.jpg",
+    opts: { special: ["top100"], groups: ["Kep1er"] },
+  },
+  {
+    name: "Yebin",
+    img: "83.jpg",
+    opts: { special: ["top100"], groups: ["DIA"] },
+  },
+  {
+    name: "Yeeun",
+    img: "84.jpg",
+    opts: { special: ["top100"], groups: ["CLC"] },
+  },
+  {
+    name: "Yeji",
+    img: "85.jpg",
+    opts: { special: ["top100"], groups: ["ITZY"] },
+  },
+  {
+    name: "Yena",
+    img: "86.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE"] },
+  },
+  {
+    name: "Yeojin",
+    img: "87.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Yeonhee",
+    img: "88.jpg",
+    opts: { special: ["top100"], groups: ["Rocket Punch"] },
+  },
+  {
+    name: "Yeonwoo",
+    img: "89.jpg",
+    opts: { special: ["top100"], groups: ["Momoland"] },
+  },
+  {
+    name: "Yiren",
+    img: "90.jpg",
+    opts: { special: ["top100"], groups: ["Everglow"] },
+  },
+  {
+    name: "YooA",
+    img: "91.jpg",
+    opts: { special: ["top100"], groups: ["Oh My Girl"] },
+  },
+  {
+    name: "Yoohyeon",
+    img: "92.jpg",
+    opts: { special: ["top100"], groups: ["Dreamcatcher"] },
+  },
+  {
+    name: "Yoon",
+    img: "93.jpg",
+    opts: { special: ["top100"], groups: ["STAYC"] },
+  },
+  {
+    name: "Yubin (ex Binnie)",
+    img: "94.jpg",
+    opts: { special: ["top100"], groups: ["Oh My Girl"] },
+  },
+  {
+    name: "Yujin",
+    img: "95.jpg",
+    opts: { special: ["top100"], groups: ["IZ*ONE", "IVE"] },
+  },
+  {
+    name: "Yukyung",
+    img: "96.jpg",
+    opts: { special: ["top100"], groups: ["Alice (ex Elris)"] },
+  },
+  {
+    name: "Yuna",
+    img: "97.jpg",
+    opts: { special: ["top100"], groups: ["ITZY"] },
+  },
+  {
+    name: "Yunkyoung",
+    img: "98.jpg",
+    opts: { special: ["top100"], groups: ["Rocket Punch"] },
+  },
+  {
+    name: "Yves",
+    img: "99.jpg",
+    opts: { special: ["top100"], groups: ["LOONA"] },
+  },
+  {
+    name: "Zoa",
+    img: "100.jpg",
+    opts: { special: ["top100"], groups: ["Weeekly"] },
+  },
+];
