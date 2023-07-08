088ce1031e74c31477e27d856c1ddae0cf21a6b5
momorin90 <ad20171217@gmail.com>
2023-07-08 11:23:17 +0200

    Removed discord awards

--- discord-awards/README.md
+++
@@ -1,3 +0,0 @@
-## How to deploy
-
-Build the project, checkout to gh-pages branch, copy the out folder into discord-awards and push.
--- discord-awards/src/router/navigation.js
+++
@@ -1,6 +0,0 @@
-export const navigation = [
-  { name: "Top Filles", href: "/" },
-  { name: "Top Garçons", href: "/topm" },
-  { name: "Top Visages", href: "/topv" },
-  { name: "Hall of Fame", href: "/pantheon" },
-];
